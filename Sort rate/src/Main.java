12a87d6f7505306af6c2deba162a99bfde0c9758
Nikita60012 <96587581+Nikita60012@users.noreply.github.com>
2022-11-17 13:33:19 +0300

    Add files via upload

---
+++ Sort rate/src/Main.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int[] arrayInput = new int[size];
+        for(int i = 0; i < size; i++){
+            arrayInput[i] = in.nextInt();
+        }
+
+        int[][] myArray = new int [2][size];
+        int currentNumber;
+        int count = 0;
+
+        for(int i = 0; i < size; i++){
+            currentNumber = arrayInput[i];
+            int arraySize = 0;
+            while(arraySize < size){
+                if(currentNumber == arrayInput[arraySize]){
+                    count++;
+                }
+                arraySize++;
+            }
+            myArray[0][i] = count;
+            myArray[1][i] = currentNumber;
+            count = 0;
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < size; j++)
+            System.out.print(myArray[i][j] + " ");
+            System.out.println();
+        }
+
+        int[] movedNumber = new int[2];
+
+        for(int i = size - 1; i >= 1; i--) {
+            for (int j = i - 1; j >= 0; j--){
+                if (myArray[0][i] > myArray[0][j]){
+                        movedNumber[0] = myArray[0][j];
+                        movedNumber[1] = myArray[1][j];
+
+                        myArray[0][j] = myArray[0][i];
+                        myArray[1][j] = myArray[1][i];
+
+                        myArray[0][i] = movedNumber[0];
+                        myArray[1][i] = movedNumber[1];
+                }
+            }
+        }
+        for(int i = 0 ; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if(myArray[0][i] == myArray[0][j])
+            }
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < size; j++)
+                System.out.print(myArray[i][j] + " ");
+            System.out.println();
+        }
+
+    }
+}
